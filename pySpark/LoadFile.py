cafb0eab26d810e53889b8f447daaf10d0b174ff
Somnath Banerjee <35511147+somnathbanerjee2020@users.noreply.github.com>
2019-02-11 14:56:34 +1100

    done1

--- pySpark/LoadFile.py
+++ pySpark/LoadFile.py
@@ -5,15 +5,24 @@ import numpy as np
 import pandas as pd
 import os
 
+#convert list to DF
+#l1={1,2,3,4,5,6}
+#df1=pd.DataFrame([l1])
+#print(df1)
+
 # creating a pandas dataframe using a test csv
 os.chdir("c:/Users/banerso/Documents/GitHub/PythonCode/pySpark/")
-
 f=open("fifa18.csv","r")
 data=pd.read_csv("fifa18.csv")
 # First x rows
-print(data.head(30))
-# k=data.tail()
-# print(k)
+sample=data.head(30)
+
+#Filtering based on a column
+print(sample[sample["Age"] > 30])
 
 # Metadata of the frame
-print(data.shape)
+#print(data.shape)
+
+
+
+
