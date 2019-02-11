728aa27d77af87f547d63f786dcf24771d90dbae
Somnath Banerjee <35511147+somnathbanerjee2020@users.noreply.github.com>
2019-02-11 11:24:09 +1100

    minor changes

--- pySpark/LoadFile.py
+++ pySpark/LoadFile.py
@@ -1,18 +1,18 @@
-#import pySpark as ps
+# import pySpark as ps
 # pandas is a dataframe library
 import pandas as pd
 import os
 import pyspark
 
-#creating a pandas dataframe using a test csv
+# creating a pandas dataframe using a test csv
 os.chdir("c:/Users/banerso/Documents/GitHub/PythonCode/pySpark/")
 
 f=open("fifa18.csv","r")
 data=pd.read_csv("fifa18.csv")
 # First x rows
 print(data.head(30))
-#k=data.tail()
-#print(k)
+# k=data.tail()
+# print(k)
 
-#Metadata of the frame
+# Metadata of the frame
 print(data.shape)
