20c798f2fac41300a6d0141ac460bf1219aac3f0
Somnath Banerjee <35511147+somnathbanerjee2020@users.noreply.github.com>
2020-08-08 19:42:35 +1000

    small changes

--- test1.py
+++ test1.py
@@ -1,19 +1,2 @@
-def unique(sequence):
-    seen = set()
-    return [x for x in sequence if not (x in seen or seen.add(x))]
-
-def merge_the_tools(string, k):
-    result=""
-    #creating t's
-    for i in range(1,len(string)+1):
-        if(i%k == 0):
-            #sub=list(set(string[0:k]))
-            w=string[0:k]
-            s=unique(w)
-            print(result.join(s))
-            string=string[k:]
-
-
-if __name__ == '__main__':
-    string, k = input(), int(input())
-    merge_the_tools(string, k)
+testName = "Arjun"
+print(f"His name is: {testName}")
