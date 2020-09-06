bd46da19a114efa2ae1a549e8073780771d63ad3
Somnath Banerjee <35511147+somnathbanerjee2020@users.noreply.github.com>
2020-09-06 21:39:42 +1000

    Update solveEuler_3.py

--- Exercise Files/SolveEuler/solveEuler_3.py
+++ Exercise Files/SolveEuler/solveEuler_3.py
@@ -1,18 +1,23 @@
-# problem 2 - https://projecteuler.net/problem=3
+# problem 3 - https://projecteuler.net/problem=3
+
+def findPrimeFactors(w):
+    for a in range(2,w):
+        if w%a == 0:
+            #print(f"{a} is a factor of {w}. Lets check if {a} is a prime")
+            k=checkPrime(a)
+            if k==1:
+                print(f'{a} is a prime factor of {w}')
+
+
 
 def checkPrime(l):
-    j=l
-    #print(f'Inside Checkprime. Checking: ',j)
-    for c in range(1,j):
-        if j%c == 0:
-            print(f'Is not a prime',j)
-            break
+    for c in range(2,l):
+        if l%c == 0:
+            return(0)
+    return(1)
     
-    return(j)
-
+    
 if __name__ == "__main__":
-    k=0
-    for i in range(1,100):
-        k=checkPrime(i)
-        #if k != 0:
-         #   print(k)
+    #k=checkPrime(24)
+    #print(k)
+    findPrimeFactors(600851475143)
